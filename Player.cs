82a6cd69eda1353488405dabd1e08834f8bbb17e
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2020-11-29 16:14:15 +0000

    Player Class with 3 properties

---
+++ Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    class Player
+    {
+        public int ID { get; set; }
+        public int MyProperty { get; set; }
+        public int Score { get; set; }
+    }
+}
